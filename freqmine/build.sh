45d82da41230fe6fb383c8d132283b9ffcb69673
ChrisYzcc <yangzechen21@mails.ucas.ac.cn>
2025-11-08 19:39:09 +0800

    add build.sh for freqmine

---
+++ freqmine/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+rm -rf build/${PLATFORM}
+
+if [ "${VERSION}" == "pthreads" ]; then
+    echo "Pthread version of Freqmine is not supported."
+    exit 1
+fi
+
+export CXXFLAGS="${CXXFLAGS} -fopenmp"
+
+mkdir -p ${PARSECDIR}/freqmine/build/${PLATFORM}/obj
+cp -r src/* ${PARSECDIR}/freqmine/build/${PLATFORM}/obj
+
+make -C ${PARSECDIR}/freqmine/build/${PLATFORM}/obj version=${VERSION}
+make -C ${PARSECDIR}/freqmine/build/${PLATFORM}/obj install version=${VERSION}
