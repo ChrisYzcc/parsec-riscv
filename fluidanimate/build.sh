e12a81c83e36d8e6506664734a08a53ca318ab5e
ChrisYzcc <yangzechen21@mails.ucas.ac.cn>
2025-11-08 19:27:58 +0800

    build for fluidanimate

---
+++ fluidanimate/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -rf build/${PLATFORM}
+
+mkdir -p ${PARSECDIR}/facesim/build/${PLATFORM}/obj
+cp -r src/* ${PARSECDIR}/facesim/build/${PLATFORM}/obj
+
+make -C ${PARSECDIR}/facesim/build/${PLATFORM}/obj version=${VERSION}
+make -C ${PARSECDIR}/facesim/build/${PLATFORM}/obj install version=${VERSION}
