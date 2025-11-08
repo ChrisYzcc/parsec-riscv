1560bbfef3c725cd6a30fdd35d91d87643e460e7
ChrisYzcc <yangzechen21@mails.ucas.ac.cn>
2025-11-08 19:13:00 +0800

    fix inplace build for facesim

--- facesim/build.sh
+++ facesim/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-
 rm -rf build/${PLATFORM}
 
-make -C src version=${VERSION}
-make -C src install version=${VERSION}
+export CXXFLAGS="${CXXFLAGS} -fexceptions"
+export PHYSBAM="${PARSECDIR}/facesim/build/${PLATFORM}/obj"
+
+mkdir -p ${PARSECDIR}/facesim/build/${PLATFORM}/obj
+cp -r src/* ${PARSECDIR}/facesim/build/${PLATFORM}/obj
+
+make -C ${PARSECDIR}/facesim/build/${PLATFORM}/obj version=${VERSION}
+make -C ${PARSECDIR}/facesim/build/${PLATFORM}/obj install version=${VERSION}
