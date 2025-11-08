ca279232f26130ab52f6ad0dd981d2edecb85a7e
ChrisYzcc <yangzechen21@mails.ucas.ac.cn>
2025-11-08 20:52:35 +0800

    add build.sh for streamcluster

---
+++ streamcluster/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm -rf build/${PLATFORM}
+
+if [ "${VERSION}" == "openmp" ]; then
+    echo "OpenMP version of Streamcluster is not supported."
+    exit 1
+fi
+
+
+mkdir -p ${PARSECDIR}/streamcluster/build/${PLATFORM}/obj
+cp -r src/* ${PARSECDIR}/streamcluster/build/${PLATFORM}/obj
+
+make -C ${PARSECDIR}/streamcluster/build/${PLATFORM}/obj version=${VERSION}
+make -C ${PARSECDIR}/streamcluster/build/${PLATFORM}/obj install version=${VERSION}
