90ebfc07d487e7db69f7c42582dfe485c0de62c5
ChrisYzcc <yangzechen21@mails.ucas.ac.cn>
2025-11-08 19:51:07 +0800

    add build.sh for swaptions

---
+++ swaptions/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm -rf build/${PLATFORM}
+
+if [ "${VERSION}" == "openmp" ]; then
+    echo "OpenMP version of Swaptions is not supported."
+    exit 1
+fi
+
+
+mkdir -p ${PARSECDIR}/swaptions/build/${PLATFORM}/obj
+cp -r src/* ${PARSECDIR}/swaptions/build/${PLATFORM}/obj
+
+make -C ${PARSECDIR}/swaptions/build/${PLATFORM}/obj version=${VERSION}
+make -C ${PARSECDIR}/swaptions/build/${PLATFORM}/obj install version=${VERSION}
