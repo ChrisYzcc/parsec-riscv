8db48b6d82ddaefea30ecc18359cadcb25cdb071
ChrisYzcc <yangzechen21@mails.ucas.ac.cn>
2025-11-08 20:14:17 +0800

    add build.sh for x264

---
+++ x264/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+BENCH_DIR=${PARSECDIR}/x264
+BUILD_DIR=${PARSECDIR}/x264/build/${PLATFORM}
+
+# Set lower optimization level to avoid segmentation faults during runtime
+export CFLAGS=" -O0 -g -funroll-loops -fprefetch-loop-arrays ${PORTABILITY_FLAGS}"
+export CXXFLAGS="-O0 -g -funroll-loops -fprefetch-loop-arrays -fpermissive -fno-exceptions ${PORTABILITY_FLAGS} -std=c++98"
+
+rm -rf build/${PLATFORM}
+
+mkdir -p ${PARSECDIR}/x264/build/${PLATFORM}/obj
+cp -r src/* ${PARSECDIR}/x264/build/${PLATFORM}/obj
+
+cd ${PARSECDIR}/x264/build/${PLATFORM}/obj
+rm -f .depend
+
+if [ "${PLATFORM}" == "rv64" ]; then
+    ./configure --prefix=${BUILD_DIR} --enable-pthread --host=riscv64-unknown-linux-gnu
+else
+    ./configure --prefix=${BUILD_DIR} --enable-pthread
+fi
+
+make version=${VERSION} -j$(nproc)
+make version=${VERSION} install
+
+# Rename
+mv ${BUILD_DIR}/bin/x264 ${BUILD_DIR}/bin/x264-${VERSION}
