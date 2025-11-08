e97f4f27b5bf3c7ff8f4167a3bb0e99a661de6ff
ChrisYzcc <yangzechen21@mails.ucas.ac.cn>
2025-11-08 16:46:12 +0800

    update build.sh for body track

--- bodytrack/build.sh
+++ bodytrack/build.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-rm -rf build/${PLATFORM}
+export CXXFLAGS="${CXXFLAGS} -fexceptions"
+
+BUILD_DIR=${PARSECDIR}/bodytrack/build/${PLATFORM}
+if [ ! -d "${BUILD_DIR}" ]; then
+    mkdir -p ${BUILD_DIR}
+fi
 
 cd src
-autoreconf -fiv
+
+VERSION_SUFFIX="threads"
+if [ "${VERSION}" == "openmp" ]; then
+    VERSION_SUFFIX="openmp"
+elif [ "${VERSION}" == "tbb" ]; then
+    VERSION_SUFFIX="tbb"
+fi
 
 if [ "${PLATFORM}" == "riscv64" ]; then
-    ./configure --prefix=${PARSECDIR}/bodytrack --enable-${VERSION} --host=riscv64-unknown-linux-gnu
+    ./configure --prefix=${BUILD_DIR} --enable-${VERSION_SUFFIX} --host=riscv64-unknown-linux-gnu
 else
-    ./configure --prefix=${PARSECDIR}/bodytrack --enable-${VERSION}
+    ./configure --prefix=${BUILD_DIR} --enable-${VERSION_SUFFIX}
 fi
+
+make version=${VERSION} clean
+make version=${VERSION} -j$(nproc)
+make version=${VERSION} install
