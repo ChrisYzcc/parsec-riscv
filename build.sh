c8388df4397f174c91a944143884969c7a74f9b3
ChrisYzcc <yangzechen21@mails.ucas.ac.cn>
2025-11-10 19:43:57 +0800

    add run.sh for ferret

--- build.sh
+++ build.sh
@@ -33,12 +33,13 @@ export EXTRA_LIBS=""
 export OPENSSL_RV_DIR="/home/yzcc/riscv64-openssl"
 export ZLIB_RV_DIR="/home/yzcc/riscv64-zlib"
 export GSL_RV_DIR="/home/yzcc/riscv64-gsl"
+export LIBJPEG_RV_DIR="/home/yzcc/riscv64-libjpeg"
 
 if [ "$PLATFORM" == "rv64" ]; then
     CROSS_COMPILE_PREFIX=riscv64-linux-gnu-
-    export CFLAGS="$CFLAGS -I${OPENSSL_RV_DIR}/include/ -I${ZLIB_RV_DIR}/include/ -I${GSL_RV_DIR}/include/"
-    export CXXFLAGS="$CXXFLAGS -I${OPENSSL_RV_DIR}/include/ -I${ZLIB_RV_DIR}/include/ -I${GSL_RV_DIR}/include/"
-    export LDFLAGS="$LDFLAGS -L${OPENSSL_RV_DIR}/lib -L${ZLIB_RV_DIR}/lib -L${GSL_RV_DIR}/lib"
+    export CFLAGS="$CFLAGS -I${OPENSSL_RV_DIR}/include/ -I${ZLIB_RV_DIR}/include/ -I${GSL_RV_DIR}/include/ -I${LIBJPEG_RV_DIR}/include/"
+    export CXXFLAGS="$CXXFLAGS -I${OPENSSL_RV_DIR}/include/ -I${ZLIB_RV_DIR}/include/ -I${GSL_RV_DIR}/include/ -I${LIBJPEG_RV_DIR}/include/"
+    export LDFLAGS="$LDFLAGS -L${OPENSSL_RV_DIR}/lib -L${ZLIB_RV_DIR}/lib -L${GSL_RV_DIR}/lib -L${LIBJPEG_RV_DIR}/lib"
 else
     CROSS_COMPILE_PREFIX=
 fi
---
+++ ferret/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+BENCH_DIR="$(pwd)"
+RUN_DIR="$(pwd)/build/${PLATFORM}/run"
+
+if [ ! -d "${RUN_DIR}" ]; then
+    mkdir -p ${RUN_DIR}
+else
+    rm -rf ${RUN_DIR}/*
+fi
+
+# Unzip input file
+echo "Unzipping input file..."
+tar -xvf inputs/input_${INPUT}.tar -C ${RUN_DIR}
+echo "Unzipping completed."
+echo
+
+case $INPUT in
+    "test") ARGS="corel lsh queries 5 5";;
+    "simdev") ARGS="corel lsh queries 50 5";;
+esac
+
+# Run ferret
+cd ${RUN_DIR}
+${BENCH_DIR}/build/${PLATFORM}/bin/ferret-${VERSION} ${ARGS} ${THREADS} output.txt
+cd ${BENCH_DIR}
