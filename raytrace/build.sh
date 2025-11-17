c7a635be5c86b2e902524bd95329944a8f9abf0b
ChrisYzcc <yangzechen21@mails.ucas.ac.cn>
2025-11-17 11:16:26 +0800

    init raytrace

---
+++ raytrace/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+BENCH_DIR=${PARSECDIR}/raytrace
+BUILD_DIR=${BENCH_DIR}/build/${PLATFORM}
+
+export CXXFLAGS="${CXXFLAGS} -fexceptions -fno-strict-aliasing -fno-align-labels -DNDEBUG -D_MM_NO_ALIGN_CHECK"
+
+if [ ! -d "${BUILD_DIR}" ]; then
+    mkdir -p ${BUILD_DIR}
+else
+    rm -rf ${BUILD_DIR}/*
+fi
+
+# Compile in tmp dir
+mkdir -p ${BUILD_DIR}/obj
+cp -r ${BENCH_DIR}/src/* ${BUILD_DIR}/obj
+cd ${BUILD_DIR}/obj
+
+VERSION_SUFFIX="threads"
+cmake -G "Unix Makefiles"\
+        -D CMAKE_PREFIX_PATH=${GNUTOOL_HOME} \
+        -D CMAKE_INSTALL_PREFIX=${BUILD_DIR} \
+        -D CMAKE_CXX_COMPILER=${CXX} \
+        -D CMAKE_CXX_FLAGS="${CXXFLAGS}" \
+        -D USE_PBOS=1 \
+        -D NEED_ARB_WRAPPERS=0 \
+        -Wno-dev \
+        .
+
+make -j$(nproc)
+make install
+
+# Rename the executable to include platform and version info
+for f in "${BUILD_DIR}/bin/"*; do
+    mv "$f" "${f}-${VERSION}"
+done
