33d067be8e0e29d62909a1c7fa43053f3f46bcd9
ChrisYzcc <yangzechen21@mails.ucas.ac.cn>
2025-11-17 11:23:27 +0800

    run raytrace

---
+++ raytrace/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+BENCH_DIR="$(pwd)"
+RUN_DIR="$(pwd)/build/${PLATFORM}/run"
+
+if [ ! -d "${RUN_DIR}" ]; then
+    mkdir -p ${RUN_DIR}
+else
+    rm -rf ${RUN_DIR}/*
+fi
+
+# Unzip input file
+echo "Unzipping input file..."
+tar -xvf inputs/input_${INPUT}.tar -C ${RUN_DIR}
+echo "Unzipping completed."
+echo
+
+case $INPUT in
+    "test") ARGS="octahedron.obj -automove -nthreads ${THREADS} -frames 1 -res 1 1";;
+    "simdev") ARGS="bunny.obj -automove -nthreads ${THREADS} -frames 1 -res 16 16";;
+esac
+
+# Run raytrace
+cd ${RUN_DIR}
+${BENCH_DIR}/build/${PLATFORM}/bin/rtview-${VERSION} ${ARGS}
+cd ${BENCH_DIR}
